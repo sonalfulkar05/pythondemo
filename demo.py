3fb43d85221ef35e28a82a3a50576c0c1cd0134c
sonalfulkar05 <sonalfulkar02@gmail.com>
2026-09-10 22:33:57 +0530

    python demo codes

---
+++ demo.py
@@ -0,0 +1,33 @@
+#a=int(input("enter a number:"))
+#print("the number is idd"+str(int(a%2!=0)))
+
+#a=int(input("enter a your age:"))
+#print(a," years = ",int(a*365)," days") 
+
+
+#a=int(input("enter the time in minutes:"))
+#b=int(a/60)
+#c=int(a%60)
+#print((a/60)," hours and ",(a%60)," minutes")
+
+#a=int(input("enter a number:"))
+#print(a,": last number is ",int(a%10))
+
+#write a programe tocheck if a person is eligible for discount the criteria is he must be a student and his age must be less than 21
+#without else if
+
+
+
+
+
+
+
+
+a=10
+b=20
+print("before swap: a=",a,"b=",b)
+a=a+b
+b=a-b
+a=a-b
+print("after swap: a=",a,"b=",b)
+
